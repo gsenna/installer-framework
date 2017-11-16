a8993027d95a644fa9391dc308f283cd3140b165
Guillermo Senna <gsenna@gmail.com>
2017-11-16 02:19:59 -0300

    fixed skip build_linux

--- push_commit.sh
+++ push_commit.sh
@@ -6,16 +6,15 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b master
+  git clone https://${GH_TOKEN}@github.com/gsenna/installer-framework.git installer-master > /dev/null 2>&1
+  cd installer-master
   echo "sdfsdfs" > Updates.xml
   git add Updates.xml
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
 
 upload_files() {
-  git remote add upstream https://${GH_TOKEN}@github.com/gsenna/installer-framework.git > /dev/null 2>&1
-  git pull upstream master
-  git push --quiet --set-upstream upstream master
+  git push
 }
 
 setup_git
